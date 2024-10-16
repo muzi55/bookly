5049f586a7d1933cae73330edefcae27ef421cde
muzi5@kakao.com <muzi5@kakao.com>
2024-10-16 11:07:02 +0900

    feat/ 헤더 뒤로가기 버튼 추가

--- src/app/components/header/Header.tsx
+++ src/app/components/header/Header.tsx
@@ -1,10 +1,11 @@
-"use client";
-
 import Link from "next/link";
+
+import HeaderButton from "./HeaderButton";
 
 export default function Header() {
 	return (
 		<header className="relative h-[44px] px-1.5">
+			<HeaderButton />
 			<Link
 				href="/"
 				className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-h3 font-bold"
--- src/app/components/header/HeaderButton.tsx
+++ src/app/components/header/HeaderButton.tsx
@@ -1,7 +1,30 @@
+"use client";
+
+import Image from "next/image";
+import { usePathname, useRouter } from "next/navigation";
 import React from "react";
 
 function HeaderButton() {
-	return <div>HeaderButton</div>;
+	const path = usePathname();
+
+	const router = useRouter();
+
+	const handleBack = () => {
+		router.back();
+	};
+
+	if (path === "/") return null;
+
+	return (
+		<button onClick={handleBack} className="w-[44px] h-[44px]">
+			<Image
+				src={"/icon/ic_back.svg"}
+				width={22}
+				height={22}
+				alt="뒤로가기 아이콘"
+			/>
+		</button>
+	);
 }
 
 export default HeaderButton;
